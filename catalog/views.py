b7e73a80adb8d5faa6523d38edcfe9049681192b
JennaFi <fi@mail.com>
2024-11-22 02:52:15 +0100

    hw 25

--- catalog/views.py
+++ catalog/views.py
@@ -8,13 +8,6 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 from catalog.forms import ProductForm
 from catalog.models import Product, Contact, Category
 
-# class AddProduct(CreateView):
-#     model = Product
-#     form = ProductForm
-#
-#     context = {'products': Product.objects.all()}
-#     template_name = 'catalog/add_product.html'
-#     success_url = '/add_product/'
 
 class ProductListView(ListView):
     model = Product
@@ -45,8 +38,7 @@ class ProductUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
-#
-#
+
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:product_list')
@@ -69,38 +61,3 @@ class ContactsView(TemplateView):
         return HttpResponse(
             f"{name}, Ваше сообщение получено<br>Телефон: {phone}<br>Сообщение: {message}"
         )
-# def home_page(request):
-#     products = Product.objects.all().order_by("-created_at")[:5]
-#     # for product in products:
-#     #     print(product.name)
-#
-#     return render(request, 'catalog/home_page.html', {'products': products})
-#
-#
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         context = {'name': name, 'phone': phone}
-#
-#         return render(request, 'catalog/feedback_form.html', context)
-#
-#     queryset = Contact.objects.all()
-#     context = {"contacts": queryset}
-#     return render(request, 'catalog/contacts.html', context)
-#
-
-# def product_detail(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     return render(request, 'catalog/product_detail.html', {'product': product},)
-#
-#
-# def product_list(request):
-#     products = Product.objects.all()
-#
-#     return render(request, 'catalog/product_list.html', {'products': products})
-#
-
-
-
